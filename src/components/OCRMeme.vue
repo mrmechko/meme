cb3ddd48ed6f83ecfd097c4fdc80e29529a61f36
Rik <rbose@knox.edu>
2021-03-29 10:59:17 -0500

    refactored

--- src/components/OCRMeme.vue
+++ src/components/OCRMeme.vue
@@ -1,9 +1,9 @@
 
 <template>
   <div class="MemeViewer">
-    {{ source }}
+    <h1> {{ source }} </h1>
     <img
-      :src=img_path
+      :src=imgPath
       alt="meme"
     >
     <h1>{{ ocrOutput }}</h1>
@@ -12,53 +12,47 @@
 
 <script>
 import Tesseract from 'tesseract.js'
-import { reactive, toRefs } from "vue"
 export default {
   name: 'OCRMeme',
-  props: {
-    source: {
-      type: String,
-      required: true
+  // props are passed to us from parents
+  props: ["source"],
+  data: function () {
+    // lets set an initial value for ocrOutput
+    return {
+      ocrOutput: "Processing...",
     }
   },
-  setup(props) {
-    // define the two props we are going to print from as reactive
-    let computedProperties = reactive({img_path: "none", ocrOutput: "Processing..."})
-
-    // turn img_path and ocrOutput into refs to the reactive variable. The function isn't going to wait for it to return, though
-    const {img_path, ocrOutput} = toRefs(computedProperties)
-
-    // get the path to the location where all the images are stored
-    let images = require.context('../assets/', false, /\.png$|\.jpg$/)
-
-    // compute the path to the source passed to us as a prop
-    let image_path = images("./"+props.source)
-
-    // update the ref for img_path to contain the computed image path
-    computedProperties["img_path"] = image_path
-
-    // now we are going to define a long-running function that will initialize and 
-    // OCR the inputted meme
-    // This function is asynchronous, meaning once it is called, the caller continues
-    // on without stopping
-    const ocr = (image_path) => Tesseract.recognize(
-        image_path, 'eng', { logger: m => computedProperties["ocrOutput"] = m.progress }
-      ).then(({data: { text } }) => { // this is a lambda!
-        // We have defined an anonymous function here. This function will be run after the async function with the output of said function
-
-        // here we are updating the ref for ocrOutput with the computed value of our ocr
-        computedProperties["ocrOutput"] = text
-      })
-    
-    // having defined the function above, run it on the image we gave it. The function isn't going to wait for it to return, though
-    ocr(image_path)
-
-    // return the reactive refs
-    // we havent waited for ocr to run
-    // this syntax is the equivalent of defining a dict in python like so:
-    // {"img_path": img_path, "ocrOutput": ocrOutput}
-    return {img_path, ocrOutput}
+  computed: {
+    // source gives us a file name, but we need to compute the exact path on the server
+    // https://vuejs.org/v2/guide/computed.html
+    imgPath : function () {
+      if (this.source !== "") {
+        let image = require.context('../assets/', false, /\.png$|\.jpg$/)("./"+this.source)
+        return image
+      } 
+      return ""
+    }
   },
+  // entries in here are called whenever the named variable changes
+  watch : {
+    // We want to change the ocrOutput whenever the file we are watching changes
+    // https://michaelnthiessen.com/how-to-watch-nested-data-vue/
+    imgPath: {
+      // The first time imgPath is computed doesn't count as a "change" so we need to specify immediate
+      immediate: true,
+      // this is the function to run when there is a change. if the function had two arguments, we could compare the new value to the previous value too
+      handler(newPath) {
+        // call our OCR api. This is an asynchronous function so we need to have a "callback" for when it finishes
+        Tesseract.recognize(
+          newPath, 'eng', { logger: m => this.ocrOutput = m.progress }
+        ).then(({data: { text } }) => { // this is a lambda!
+          // We have defined an anonymous function here. This function will be run after the async function with the output of said function
+          // here we are updating the ref for ocrOutput with the computed value of our ocr
+          this.ocrOutput = text
+        })
+      }
+    }
+  }
 }
 </script>
 
